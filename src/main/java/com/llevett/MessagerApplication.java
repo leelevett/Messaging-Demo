96e36006474c8791ab0f7b308bb4bc4e06c1e66d
Lee Levett <lee_levett@hotmail.com>
2017-04-14 18:19:05 +0100

    First commit

---
+++ src/main/java/com/llevett/MessagerApplication.java
@@ -0,0 +1,12 @@
+package com.llevett;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagerApplication.class, args);
+	}
+}
