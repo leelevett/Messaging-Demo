dabe105bbef134d42f13c1bc8080e64c7c33f1b1
Lee Levett <lee_levett@hotmail.com>
2017-09-23 19:22:15 +0100

    Final update for post 1

--- src/main/java/com/llevett/spanish/SpanishHelloWorld.java
+++ src/main/java/com/llevett/spanish/SpanishHelloWorld.java
@@ -3,7 +3,6 @@ package com.llevett.spanish;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpanishHelloWorld implements CommandLineRunner {
